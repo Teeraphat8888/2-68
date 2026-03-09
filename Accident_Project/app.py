086fa546b6d0da03f0b4e797c58d75126f24c800
Teeraphat <mewcup2548@gmail.com>
2026-03-09 22:15:18 +0700

    login

--- Accident_Project/app.py
+++ Accident_Project/app.py
@@ -90,7 +90,7 @@ if 'show_login' not in st.session_state:
 
 with st.sidebar:
     st.image("Accident_Project/download.png", width=100)
-    st.title("ระบบเจ้าหน้าที่")
+    st.title("เข้าสู่ระบบ(เฉพาะเจ้าหน้าที่)")
     
     if not st.session_state['logged_in']:
         if not st.session_state['show_login']:
